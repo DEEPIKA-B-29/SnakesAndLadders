1d34a3ed7c07c3b357f1be0906239bc91db80594
DEEPIKA-B-29 <deepubalu2021@gmail.com>
2025-12-11 15:33:16 +0530

    UC3

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,7 +1,23 @@
 public class SnakeLadder {
-    int startPosition=0;
-    static int dice=(int) (Math.random()*6);
+    static int startPosition=0;
+
     static void display() {
-        System.out.println(dice);
+
+            int dice=(int) (Math.random()*6)+1;
+            System.out.println("Dice: "+dice);
+
+            int option = (int) (Math.random() * 3) + 1;
+            System.out.println("Option: " + option);
+
+            if (option == 1) {
+            }
+            else if (option == 2) {
+                startPosition += dice;
+            }
+            else if (option == 3) {
+                    startPosition -= dice;
+            }
+            System.out.println("Position: "+startPosition);
+
     }
 }
