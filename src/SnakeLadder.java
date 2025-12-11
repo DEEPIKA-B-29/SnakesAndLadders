d72021eb92a5325173d035179034e45030cb97fd
DEEPIKA-B-29 <deepubalu2021@gmail.com>
2025-12-11 14:48:26 +0530

    UC2

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -2,6 +2,6 @@ public class SnakeLadder {
     int startPosition=0;
     static int dice=(int) (Math.random()*6);
     static void display() {
-        //System.out.println(dice);
+        System.out.println(dice);
     }
 }
