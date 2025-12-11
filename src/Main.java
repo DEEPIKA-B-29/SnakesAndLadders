17687a970403c6cc6048097f05de01ca0caeb4cd
DEEPIKA-B-29 <deepubalu2021@gmail.com>
2025-12-11 16:34:12 +0530

    UC7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
-public class Main{
-    public static void main(String[] args){
-    SnakeLadder.display();
+public class Main {
+    public static void main(String[] args) {
+
+        SnakeLadder player1 = new SnakeLadder();
+        SnakeLadder player2 = new SnakeLadder();
+
+        int currentPlayer = 1;
+
+        while (true) {
+
+            if (currentPlayer == 1) {
+                System.out.println(" \nPLAYER 1 TURN ");
+                boolean ladder = player1.display();
+                if (player1.startPosition == 100) {
+                    System.out.println("PLAYER 1 WON!");
+                    break;
+                }
+                if (!ladder) {
+                    currentPlayer = 2;
+                }
+            } else {
+                System.out.println("  \nPLAYER 2 TURN");
+                boolean ladder = player2.display();
+                if (player2.startPosition == 100) {
+                    System.out.println("PLAYER 2 WON!");
+                    break;
+                }
+                if (!ladder) {
+                    currentPlayer = 1;
+                }
+            }
+        }
     }
 }
--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,36 +1,39 @@
 public class SnakeLadder {
-    static int startPosition=0;
 
-    static void display() {
+    int startPosition = 0;
+    int numberOfDiceRoll = 0;
 
-        while (startPosition>=0 && startPosition<100) {
+    boolean display() {
 
-            int dice=(int) (Math.random()*6)+1;
-            System.out.println("Dice: "+dice);
+        int dice = (int) (Math.random() * 6) + 1;
+        numberOfDiceRoll++;
+        System.out.println("Dice: " + dice);
 
-            int option = (int) (Math.random() * 3) + 1;
-            System.out.println("Option: " + option);
+        int option = (int) (Math.random() * 3) + 1;
+        System.out.println("Option: " + option);
 
-            if (option == 1) {
-                continue;
-            }
-
-            else if (option == 2)
-            {
-                startPosition += dice;
-                if (startPosition>100){
-                    startPosition-=dice;
-                }
-            }
-
-            else if (option == 3) {
-                if (startPosition > dice) {
-                    startPosition -= dice;
-                } else {
-                    startPosition = 0;
-                }
-            }
-            System.out.println("Position: "+startPosition);
+        if (option==1) {
+            System.out.println("Position: " + startPosition);
+            return false;
         }
+
+        else if (option==2) {
+            startPosition+=dice;
+            if (startPosition>100) {
+                startPosition-=dice;
+            }
+            System.out.println("Position: " + startPosition);
+            return true;
+        }
+        else if (option==3) {
+            if (startPosition > dice) {
+                startPosition -= dice;
+            } else {
+                startPosition = 0;
+            }
+        }
+        System.out.println("Position: " + startPosition);
+        System.out.println("Number of dice rolls: "+numberOfDiceRoll);
+        return false;
     }
 }
